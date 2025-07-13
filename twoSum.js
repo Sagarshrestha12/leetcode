f6ff5aab299fc6ebfddf6dc7c5012e091e859b5a
SagarShrestha12 <sagarshrestha4339@gmail.com>
2025-07-13 17:28:26 +1000

    solving the two sum problem using the binary search algorithm and searching the target from index to array.length.

--- twoSum.js
+++ twoSum.js
@@ -3,14 +3,63 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    for(let i = 0;i <= nums.length-1; i++ ){
-        for(let j = i+1; j<= nums.length; j++){
-            if(nums[i]+nums[j]=== target){
-                return [i,j];
-            }
+
+// This is a solution to the Two Sum problem using a brute force approach.
+// var twoSum = function(nums, target) {
+//     for(let i = 0;i <= nums.length-1; i++ ){
+//         for(let j = i+1; j<= nums.length; j++){
+//             if(nums[i]+nums[j]=== target){
+//                 return [i,j];
+//             }
+//         }
+//     }
+// };
+
+function isTargetOfNumFound(targetArr, startIndex, endIndex, targetNum) {
+//   let startIndex = stra
+let startIndexing = startIndex; 
+  while (startIndex <= endIndex && startIndex >= startIndexing) {
+    let mid = Math.floor((startIndex + endIndex) / 2);
+
+    if (targetArr[mid] === targetNum) {
+      return true; 
+    } else if (targetNum < targetArr[mid]) {
+      endIndex = mid - 1; 
+    } else {
+      startIndex = mid + 1; 
+    }
+  }
+
+  return false;
+}
+
+
+var findIndexOfElement = function (nums,num, indexI){
+ for(let i =0; i< nums.length; i++){
+    if(indexI === -1 && nums[i]=== num){
+        return i; 
+    }
+    else if(nums[i]=== num && indexI !== i){
+        return i; 
+    }
+ }
+ return 0; 
+}
+
+var twoSum = function(nums, target){
+
+   let sortArray = [...nums].sort((a,b)=> a-b);
+
+    for(let n =0; n < sortArray.length; n++ ){
+        let num = sortArray[n];
+        let numy  = target - num; 
+        if(isTargetOfNumFound(sortArray, n+1, nums.length-1, numy)){
+            
+            let i = findIndexOfElement(nums,num, -1); 
+            return [i,findIndexOfElement(nums,numy,i )]
         }
     }
-};
-console.log(twoSum([2,7,11,15], 9)); // [0,1]
-// Time Complexity: O(n^2)
+    return [1,1]
+}
+
+
